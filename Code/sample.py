a5c4fc6cd3ef396b06a1383fc15a24fe6e655bdf
nickKearns <nickkearns133@gmail.com>
2019-10-31 20:23:27 -0700

    added stochastic sampling

--- Code/sample.py
+++ Code/sample.py
@@ -1,4 +1,31 @@
 import random
+import sys
+from histogram import histogram
+
+
+def sample(source_text):
+    hist = histogram(source_text)
+    upper_bound = 0
+    for value in hist.values():
+        upper_bound += value
+    ran_num = random.randint(1, upper_bound)
+    total = 1
+    for key, value in hist.items():
+        total += value
+        if total >= ran_num:
+            return key
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    source_text = sys.argv[1]
+    histo = histogram(source_text)
+    print(sample(source_text))
 
 
 
